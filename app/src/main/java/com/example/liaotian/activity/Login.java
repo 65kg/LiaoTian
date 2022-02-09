610c2935ca05e531b34b0188918d72645691f68b
65kg <1401671966@qq.com>
2022-02-09 20:09:33 +0800

    1.0.0

--- app/src/main/java/com/example/liaotian/activity/Login.java
+++ app/src/main/java/com/example/liaotian/activity/Login.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 
 import com.example.liaotian.R;
@@ -16,17 +18,30 @@ import com.example.liaotian.R;
  * 登录界面
  */
 public class Login extends AppCompatActivity {
+    private NavController controller;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
-
-
-
+        controller = Navigation.findNavController(this,R.id.fragment);
     }
 
 
+    /**
+     * 判断当前fragment是否为xx如果是按返回键直接退出程序
+     *如果不是就正常返回上一个页面
+     */
+    @Override
+    public void onBackPressed() {
+        if (controller.getCurrentDestination().getId()  == R.id.loginFragment){
+            finish();
+
+        }else {
+
+            super.onBackPressed();
+        }
+
+    }
 
 }
